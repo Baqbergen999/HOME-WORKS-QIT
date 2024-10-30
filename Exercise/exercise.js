1978f5a4ae8ab6ad08737b6b802a2dbbffbe84bf
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-10-30 17:20:36 +0600

    add the folder named exercise

---
+++ Exercise/exercise.js
@@ -0,0 +1,11 @@
+let name = "Baqbergen";
+let surname = "Nurqadyr";
+const birthday = "24.07.2011";
+let school = "172-gymnasium"
+let weight = "40kg";
+
+console.log(name);
+console.log(surname);
+console.log(birthday);
+console.log(school);
+console.log(weight);
