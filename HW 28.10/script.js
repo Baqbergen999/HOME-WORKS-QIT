a2b3d9bf22d05022366004a12a4ff8d19d0c086e
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-10-30 16:09:06 +0600

    add nothing

--- HW 28.10/script.js
+++ HW 28.10/script.js
@@ -1,4 +1,4 @@
 console.log("1) JavaScript — бұл веб-беттерге интерактивтілік қосуға арналған бағдарламалау тілі.");
 console.log("2) Комментарий қосу үшін 'Ctrl + /' деген батырмаларды басу керек.");
 console.log("3) Бүгінгі датаны алу үшін <p id='date'></p> <script> const today = new Date(); document.getElementById('date').innerHTML = today.toDateString(); </script>")
-console.log("4) External және Internal")
+console.log("4) External және Internal") 
