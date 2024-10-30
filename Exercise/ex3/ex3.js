f7425f7354f61b026c6e3f15f6e5cc11a7818220
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-10-30 17:47:48 +0600

    add the folder named ex3

---
+++ Exercise/ex3/ex3.js
@@ -0,0 +1,7 @@
+let surname = "Nurqadyr";
+let relatives = ["Aitzhan", "Nurgul", "Lazzat", "Baqbergen", "Nurtileu", "Aiganym"];
+let relativesNumber = "6";
+
+console.log(surname);
+console.log(relatives);
+console.log(relativesNumber);
