1b8bae5658dec166623d1f63f295cdbd21c279fa
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-10-31 21:49:33 +0600

    add a homework named 31-10

---
+++ HW 31.10/31-10.js
@@ -0,0 +1,28 @@
+console.log("1) Оқиға");
+
+let slicesPerPizza = 8;
+let numberOfPizzas = 5;
+let totalSlices = numberOfPizzas * slicesPerPizza;
+
+console.log(totalSlices);
+
+
+
+
+console.log("2) Оқиға");
+
+let numberOfFriends = 6;
+let totalChocolates = 24;
+let chocolatesPerFriend = totalChocolates / numberOfFriends;
+
+console.log(chocolatesPerFriend);
+
+
+
+
+console.log("3) Оқиға");
+
+plantHeight = 10;
+plantHeight += 2;
+
+console.log(plantHeight);
