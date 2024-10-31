963e3285dd14e4abb4ef451228d84e2441ba2c2e
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-10-31 10:00:13 +0600

    add the homework named "HW 30.10"

---
+++ HW 30.10/30-10.js
@@ -0,0 +1,13 @@
+let modelName = "Matiz";
+let color = "Greenyellow";
+let wheelsCount = 4;
+let isOffRoad = "Жолсыз аймаққа арналмаған"
+let rating = "4.8 балл (45 орын)"
+
+
+
+console.log(modelName)
+console.log(color)
+console.log(wheelsCount)
+console.log(isOffRoad)
+console.log(rating)
