5cd37b03688ee85c125839d2c5e9ef758a176d62
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-11-04 20:44:32 +0600

    add a homework named 03-11

---
+++ HW 03.11/03-11.js
@@ -0,0 +1,46 @@
+console.log("1-тапсырма");
+
+let age = 22;
+console.log(age > 16);
+
+console.log("2-тапсырма");
+
+let num = 7;
+if (num % 2 == 0) {
+    console.log("  Жұп сан");
+}
+else {
+    console.log("  Тақ сан")
+}
+
+console.log("2-тапсырма");
+
+let temp = 11;
+console.log(temp <= 30 && temp >= 20);
+
+console.log("4-тапсырма");
+
+let num2 = -1;
+if (num2 >= 0) {
+    console.log("  Оң сан");
+    
+}
+else {
+    console.log("  Теріс сан");
+    
+}
+
+console.log("5-тапсырма");
+
+let num3 = 20;
+if (num3 % 5 == 0 && num3 % 10 == 0) {
+    console.log("Шын");
+    
+}
+else {
+    console.log("Жалған");
+    
+}
+
+
+
