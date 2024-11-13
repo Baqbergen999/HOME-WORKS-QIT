7dcea2d43d1b7a3def682fe850a2ad5187fb8537
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-11-13 21:48:04 +0600

    add the homeork named 13.11

--- HW 13.11/13.11.js
+++ HW 13.11/13.11.js
@@ -56,19 +56,19 @@ console.log("Ex3");
 
 let start = Number(prompt("Санның басталатын санын енгізіңіз:"));
 let end = Number(prompt("Санның аяқталатың санын енгізіңіз:"));
-let result4 = 0;
-let result7 = 0;
+let result4 = 1;
+let result7 = 1;
 
 do {
     if (start % 4 == 0) {
-        result4 = result4 + start;
+        result4 = result4 * start;
     } else if (start % 7 == 0) {
-        result7 = result7 + start;
+        result7 = result7 * start;
     } else {
         console.log(start);
     }
     start++;
     
 } while (start <= end);
-console.log("4-ке қалдықсыз бөлінетін сандардың қосындысы: " + result4);
-console.log("7-ге қалдықсыз бөлінетін сандардың қосындысы: " + result7);
+console.log("4-ке қалдықсыз бөлінетін сандардың көбейтіндісі: " + result4);
+console.log("7-ге қалдықсыз бөлінетін сандардың көбейтіндісі: " + result7);
