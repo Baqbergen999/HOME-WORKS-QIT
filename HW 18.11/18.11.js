ecc87502846ca6d1d7ff3aae7826587bfd25b577
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-11-18 21:52:15 +0600

    add a Homework named 18.11

---
+++ HW 18.11/18.11.js
@@ -0,0 +1,10 @@
+let num = +prompt("Enter any number:");
+
+function taqJup(){
+    if (num % 2 == 0) {
+        return "Zhup san";
+    } else {
+        return "Taq san";
+    }
+}
+console.log(taqJup());
