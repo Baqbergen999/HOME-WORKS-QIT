1f4fa7795262da61eae085fb434daf28bcfd771b
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-11-29 18:45:50 +0600

    add the home work named HW 28.11

---
+++ HW 28.11/28.11.js
@@ -0,0 +1,10 @@
+let sandar = [2, 4, 6, 8];
+let result = sandar.map((num) => num * 3);
+console.log(result);
+
+
+
+
+let numbers = [3, 7, 1, 9, 4];
+let bigFive = numbers.filter((nums) => nums >= 5);
+console.log(bigFive);
