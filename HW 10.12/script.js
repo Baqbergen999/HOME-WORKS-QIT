f06273526446c609500a53c8e2ebb9e5f7e98bb7
Nurkadyr Bakbergen <bahbergen998@gmail.com>
2024-12-10 22:17:20 +0600

    add homework named HW 10.12

---
+++ HW 10.12/script.js
@@ -0,0 +1,14 @@
+const title = document.querySelector("#title");
+title.innerHTML = "<strong>Спорт әлеміне қош келдіңіз!</strong>";  
+
+const information = document.querySelector("#information");
+information.textContent = "Спорт - белгілі ережелеге сай жарыс күйінде өтетін әрекет.";
+
+const quote = document.querySelector("#quote");
+quote.textContent = "Спорт – кермек тер шығаратын ермек.";
+
+const fact = document.querySelector("#fact");
+fact.innerHTML = "<em>Спорт туралы қызықты бір факт: спорттың еңтанымал түрі бокстың тарихы шамамен 3000 жыл бұрын Мысырда басталған</em>";
+
+const name = document.querySelector("#name");
+name.textContent = "Менің атым 'Нұрқадыр Бақберген'";
